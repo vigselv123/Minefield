6e4f638923a6e30a17f7fc1ca42bc0e08d459d40
bhagatjeetd <dbhagatjeet@gmail.com>
2025-03-14 16:20:48 -0700

    Update MineFieldController.java

--- src/minefield/MineFieldController.java
+++ src/minefield/MineFieldController.java
@@ -1,4 +1,18 @@
 package minefield;
 
-public class MineFieldController {
+import javax.swing.*;
+import java.awt.*;
+
+//simple controller panel that extends AppPanel. 
+//It adds directional buttons to left side, and uses AppPanel's menu for File/Edit operations.
+public class MineFieldController extends AppPanel {
+
+    public MineFieldController(AppFactory factory) {
+        super(factory);
+        layoutView();
+    }
+  //Creates panel of directional buttons on the west side and puts view in center
+    protected void layoutView() {
+        
+    }
 }
